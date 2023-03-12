7a3bea166312e20caeb00a2cb44d8312ea78b9b7
thedovla <donjakvlado@gmail.com>
2023-03-12 19:51:00 +0100

    Thread, TCP, ClickListener

--- app/src/main/java/com/example/einzelbeispielse2/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispielse2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.einzelbeispielse2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -18,9 +19,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         Text = findViewById(R.id.textView2);
         Result = findViewById(R.id.textView3);
         Matrikelnummer = findViewById(R.id.inputText);
         SendMNr = findViewById(R.id.button);
+
+        SendMNr.setOnClickListener(new View.OnClickListener() {
+            String result = Matrikelnummer.getText().toString();
+            @Override
+            public void onClick(View v) {
+                TCP client = new TCP(result);
+                Thread t1 = new Thread(client);
+                t1.start();
+                try {
+                    t1.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                Result.setText(client.getModifiedSentence());
+            }
+        });
     }
 }
--- app/src/main/java/com/example/einzelbeispielse2/TCP.java
+++ app/src/main/java/com/example/einzelbeispielse2/TCP.java
@@ -4,16 +4,28 @@ import java.io.*;
 import java.net.*;
 
 public class TCP extends Thread {
-    public void run(String MNr) throws Exception {
-        String modifiedSentence;
-        BufferedReader inFormUser = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
-        DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader inFormServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        outputStream.writeBytes(MNr + '\n');
-        modifiedSentence = inFormServer.readLine();
-        System.out.println("FROM SERVER: " + modifiedSentence);
-        clientSocket.close();
+    String MNr;
+    String modifiedSentence;
+    public TCP(String MNr){
+        this.MNr = MNr;
+    }
+
+    public String getModifiedSentence() {
+        return modifiedSentence;
+    }
+
+    public void run()  {
+        try {
+            BufferedReader inFormUser = new BufferedReader(new InputStreamReader(System.in));
+            Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+            DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFormServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            outputStream.writeBytes(MNr + '\n');
+            modifiedSentence = inFormServer.readLine();
+            System.out.println("FROM SERVER: " + modifiedSentence);
+            clientSocket.close();
+            } catch (IOException e) {
+            throw new RuntimeException(e);}
 
     }
 }
