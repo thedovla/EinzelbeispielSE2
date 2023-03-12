7308ac64ab117ac1663483b6047a61e37b064ec5
thedovla <donjakvlado@gmail.com>
2023-03-12 15:21:01 +0100

    Declared variables in MainActivity, TCP class finished

--- app/src/main/java/com/example/einzelbeispielse2/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispielse2/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.einzelbeispielse2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView Text;
+    private TextView Result;
+    private EditText Matrikelnummer;
+    private Button SendMNr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Text = findViewById(R.id.textView2);
+        Result = findViewById(R.id.textView3);
+        Matrikelnummer = findViewById(R.id.inputText);
+        SendMNr = findViewById(R.id.button);
     }
 }
